ae540ecd6444e9714043fe820080456508d4eb45
Bar Goldinfeld <bargoldinfeld@gmail.com>
2018-09-23 18:02:54 +0300

    basic models

---
+++ HamburgersBlog/Models/Restaurant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HamburgersBlog.Models
+{
+    public enum Area
+    {
+        Hazafon, Hadarom, Hamerkaz,
+    }
+
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        [Required]
+        public Area Area { get; set; }
+        public bool IsVeganFriendly { get; set; }
+        public bool IsKosher { get; set; }
+        public bool IsParkingAvailable { get; set; }
+
+        public virtual ICollection<Hamburger> Hamburgers { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ HamburgersBlog/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HamburgersBlog.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public int RestaurantID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string AuthorName { get; set; }
+        public string Content { get; set; }
+    }
+}

7ac1e7f59f5acbe7434429f1ba0db070c8143d11
Bar Goldinfeld <bargoldinfeld@gmail.com>
2018-10-13 17:08:04 +0300

    feat(admin): Comments, reviews and twitter (config and controllers)

---
+++ HamburgersBlog/Controllers/ReviewsController.cs
@@ -0,0 +1,120 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HamburgersBlog.DAL;
+using HamburgersBlog.Models;
+
+namespace HamburgersBlog.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private ProjectContext db = new ProjectContext();
+
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var reviews = db.Reviews;
+            return View(reviews.ToList());
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            ViewBag.ResturantId = new SelectList(db.Restaurants, "ResturantId", "Name");
+            return View();
+        }
+
+        // POST: Review/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID,Title,AuthorName,Content")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(review);
+                db.SaveChanges();
+
+                return RedirectToAction("../Restaurants/Details", new { id = review.RestaurantID });
+            }
+            return View(review);
+        }
+
+        // GET: Posts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Users = "Admin")]
+        public ActionResult Edit([Bind(Include = "ReviewID,Title,AuthorName,RestaurantID,Content")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("../Restaurants/Details", new { id = review.RestaurantID });
+            }
+            return View(review);
+        }
+
+        // GET: Posts/Delete/5
+        [Authorize(Users = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Users = "Admin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("../Restaurants/Details", new { id = review.RestaurantID });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HamburgersBlog/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tweetinvi;
+
+[assembly: OwinStartup(typeof(HamburgersBlog.Startup))]
+
+namespace HamburgersBlog
+{
+    public class Startup
+    {
+        private const string TWITTER_CONSUMER_ID = "IQOYpID8gcSwS2Ogm3anAhrDZ";
+        private const string TWITTER_CONSUMER_SECRET = "W3Dqh3kBOvJVXckH8qJEwDdnz4QDpeDYluBQhOYXBUdHvTzRVA";
+        private const string TWITTER_ACCESS_TOKEN = "1043517806443200512-ZQBv19QESwj3Xl5dH7gZr5ZrN2JJiM";
+        private const string TWITTER_ACCESS_TOKEN_SECRET = "zzxOWcL6vJzwC4MC3jJq7w0CxXM2dyC53ex1gB4P2ac5q";
+
+        public void Configuration(IAppBuilder app)
+        {
+            Auth.SetUserCredentials(TWITTER_CONSUMER_ID,
+                                    TWITTER_CONSUMER_SECRET,
+                                    TWITTER_ACCESS_TOKEN,
+                                    TWITTER_ACCESS_TOKEN_SECRET);
+        }
+    }
+}
